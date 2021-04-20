f234037f3c7878f82266ddf02ce36fae675950f6
liushirong <1024269771@qq.com>
2021-04-20 22:25:09 +0800

    获取最新的LPR输入POC系统

---
+++ emails/email_manage.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class EmailManage:
+
+    def send_email(self):
+
+        #定义SMTP服务器
+        smtpserver = 'smtp.qq.com'
+        #发送邮件的用户名合密码
+        username = "1024269771@qq.com"
+        password = "zhftjoormxbpbcgf"
+        #接受邮件的邮箱
+        receiver = "1549902501@qq.com"
+        #创建邮件对象
+        message = MIMEMultipart("related")
+        subject = "最新LPR数据"    #邮件的主题
+        fujian = MIMEText(open("report.html","rb").read(),"html","utf-8")
+        #邮件信息组装到邮件对象里面
+        message["form"] = username
+        message["to"] = receiver
+        message["subject"] = subject
+        message.attach(fujian)
+        #登录smtp服务器发送邮件
+        smtp = smtplib.SMTP()
+        smtp.connect(smtpserver)
+        smtp.login(username,password)
+        smtp.sendmail(username,receiver,message.as_string())
+        smtp.quit()
+if __name__ == '__main__':
+
+    EmailManage().send_email()
